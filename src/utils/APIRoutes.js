d6cebc95faf152da6b0e7e5115590d4050ad4258
Sabarish Chandrasekaran <sabarish27k6@gmail.com>
2022-07-29 11:20:54 +0530

    first commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,6 @@
+export const host = "http://localhost:5000";
+
+export const loginRoute = `${host}/user/login`;
+export const registerRoute = `${host}/user/register`;
+export const forgetRoute = `${host}/user/forget-password`;
+export const resetRoute = `${host}/user/reset-password`;
